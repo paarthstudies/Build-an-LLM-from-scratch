3f487dc237e0f779c49e0e8a5deeb0247a46b7c8
paarthstudies <paarthstudies@gmail.com>
2026-07-17 15:31:24 +0530

    chapter 5 added

--- ide/chapter2.py
+++ ide/chapter2.py
@@ -57,10 +57,10 @@ class GPTDatasetV1(Dataset):
         self.target_ids = []
         token_ids = tokenizer.encode(txt)
         for i in range(0, len(token_ids) - max_length, stride):
-        input_chunk = token_ids[i:i + max_length]
-        target_chunk = token_ids[i + 1: i + max_length + 1]
-        self.input_ids.append(torch.tensor(input_chunk))
-        self.target_ids.append(torch.tensor(target_chunk))
+            input_chunk = token_ids[i:i + max_length]
+            target_chunk = token_ids[i + 1: i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
     def __len__(self):
         return len(self.input_ids)
     def __getitem__(self, idx):
